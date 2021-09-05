7e6042955cee1ab067c1748524d8fdf4b2988b0b
alex-j-k <63852006+alex-j-k@users.noreply.github.com>
2021-09-05 23:54:12 +1000

    fin

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,4 +1,6 @@
-const key  = 'tFlfX5ZFpnWErhD84pVb2Vhrb4J66IW4';
+// INTERACT WITH API
+
+const key  = 'iZIhBZcZMwV8xBEjoucy0ZTJe6J33XUx';
 
 // GET WEATHER INFO
 
@@ -7,10 +9,10 @@ const getWeather = async (id) => {
 const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
 const query = `${id}?apikey=${key}`;
 
-const response  = await fetch(base + query);
+const response = await fetch(base + query);
 const data = await response.json();
 
-return data[0]
+return data[0];
 
 };
 
@@ -28,11 +30,135 @@ return data[0];
 };
 
 
-getCity('manchester')
-    .then(data => { 
-       return getWeather(data.Key); 
-    }).then(data => console.log(data))
-    .catch(err => console.log(err));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // get weather information
+// const getWeather = async (id) => {
+  
+//     const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+//     const query = `${id}?apikey=${key}`;
+  
+//     const response = await fetch(base + query);
+//     const data = await response.json();
+  
+//     return data[0];
+  
+//   };
+  
+//   // get city information
+//   const getCity = async (city) => {
+  
+//     const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+//     const query = `?apikey=${key}&q=${city}`;
+  
+//     const response = await fetch(base + query);
+//     const data = await response.json();
+  
+//     return data[0];
+  
+//   };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// getCity('manchester')
+//     .then(data => { 
+//        return getWeather(data.Key); 
+//     }).then(data => console.log(data))
+//     .catch(err => console.log(err));
 
 
 
